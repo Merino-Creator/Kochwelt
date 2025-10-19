dbdbbb52a43c39388504aa2cdd12e837c9bd5dfd
Daniel <stuermer.dan137@gmail.com>
2025-10-19 19:30:54 +0200

    added random function to all pages

--- contactFormular/contactScript.js
+++ contactFormular/contactScript.js
@@ -14,3 +14,18 @@ function sendMail(event){
         console.log(error);
     });
 } 
+
+function randomRecipe() {
+    const pages = [
+      "/recipeMemberOne/recipeMemberOne.html",
+      "/recipeMemberTwo/recipeMemberTwo.html",
+      "/recipeMemberThree/recipeMemberThree.html",
+      "/extraRecipeOne/extraRecipeOne.html",
+      "/extraRecipeTwo/extraRecipeTwo.html"
+    ];
+
+    const randomIndex = Math.floor(Math.random() * pages.length);
+    const randomPage = pages[randomIndex];
+
+    window.location.href = randomPage;
+  }
